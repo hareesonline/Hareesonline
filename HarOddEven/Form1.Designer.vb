f556caef32586ae20e26745746770ef18eb6162e
Harees Salih <168330102+hareesonline@users.noreply.github.com>
2025-01-21 14:56:09 +0300

    Add files via upload
    HarOddEven

---
+++ HarOddEven/Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        Label1 = New Label()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(107, 32)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(174, 23)
+        TextBox1.TabIndex = 0
+        TextBox1.Text = "Enter The Number to Check"
+        TextBox1.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(161, 72)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 1
+        Button1.Text = "Check "
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(176, 119)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(44, 15)
+        Label1.TabIndex = 2
+        Label1.Text = "Results"
+        Label1.TextAlign = ContentAlignment.TopCenter
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(436, 256)
+        Controls.Add(Label1)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        MaximizeBox = False
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Odd or Even"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+
+End Class
---
+++ HarOddEven/Form1.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Reflection.Emit
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim number As Integer
+
+        ' Try to parse the input as an integer
+        If Integer.TryParse(TextBox1.Text, number) Then
+            ' Check if the number is even or odd
+            If number Mod 2 = 0 Then
+                Label1.Text = "The number is even."
+            Else
+                Label1.Text = "The number is odd."
+            End If
+        Else
+            Label1.Text = "Please enter a valid integer."
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
